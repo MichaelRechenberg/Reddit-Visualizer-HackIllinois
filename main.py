47229afa597a661b0f90bd5d449b06fb91f4bd10
MichaelRechenberg <mikerechenberg@owc.net>
2016-02-20 03:24:56 -0600

    Now the user can enter a word and have it search the front page comments of the random subreddit

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
+# Reddit visualizer for HackIllinois 2016
+#
+# Author: Michael Rechenberg
+
 import praw
+import re
 
 print "Starting\n"
 
@@ -7,19 +12,21 @@ user_agent = "HackIllinois 2016 Reddit Scraper u/MikeandOrIke"
 #Make our Reddit object with the string as our user agent
 r = praw.Reddit(user_agent)
 
-#Get the ID's of the page's threads
-url = "https://www.reddit.com/r/uiuc/.json"
+#get a random subreddit
+randomSub = r.get_random_subreddit(nsfw=False)
+print randomSub
+
+#Get the ID's of the subreddit's front page
+url = "https://www.reddit.com/r/{0}/.json".format(str(randomSub))
 derp = r.request(url)
 #dictionary holding our json
 data = derp.json()
 
+#Add all the thread id's of the front page of the
+# subreddit to idArr
 idArr = []
-
 for x in data['data']['children']:
     idArr.append(x['data']['id'])
-
-
-#Access html
 
 # req = urllib2.Request(url, headers={'User-Agent': user_agent})
 # html = urllib2.urlopen(req).read()
@@ -35,22 +42,26 @@ for subID in idArr:
     #make it an unordered list instead of a tree
     comments = praw.helpers.flatten_tree(submission.comments)
 
-    #write the comment to a text file
     for x in comments:
         commentList.append(x.body)
 
-for com in commentList:
-    print com
-randomSub = r.get_random_subreddit(nsfw=False)
-print randomSub
+
+keyword = raw_input("Enter the word you want to search for (Case Insensitive)\n")
+
+#count how many times the word was found in the comments
+#only counts one word per comment to prevent spammers from
+#   skewing results
+#use re's findall if I want to count multiple words
+wordCount = 0
+
+for comment in commentList:
+    comment = comment.lower()
+    if re.search(r"\b{0}\b".format(keyword), comment) != None:
+        print comment
+        wordCount += 1
+
+print wordCount
 
 
-subreddit = r.get_subreddit(str(randomSub))
 
-# all_comments = r.get_comments("funny", limit=0)
-# for x in all_comments:
-#     print x.body
-
-
-
-print "done"
+print "Finished!"
